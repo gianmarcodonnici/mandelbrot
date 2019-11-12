06d7025b2c9f09408940886e6f324f1f25e317f0
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-11-12 14:59:37 +0100

    wrote render function

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,30 @@ fn pixel_to_point(  bounds: (usize, usize),
     }
 }
 
+/// Render the rectangle of the mandelbrot set into a buffer of pixels
+/// 'bounds' is the width and height of the buffer 'pixels'
+/// which holds greyscale pixels.
+/// 'upper_left' and 'lower_right' specify the points on the complex plane corresponding to
+/// the corners of the pixel buffer
+fn render(pixels: &mut [u8],
+            bounds: (usize, usize),
+            upper_left: Complex<f64>,
+            lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    
+    for row in 0..bounds.1 {
+        for column in 0.bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8 
+
+            };
+        }
+    }
+}
+
 
 //Tests below
 #[test]
@@ -82,7 +106,6 @@ fn test_pixel_to_point() {
                               Complex { re:  1.0, im: -1.0 }),
                Complex { re: -0.5, im: -0.5 });
 }
-
 
 fn main() {
     println!("Hello, world!");
