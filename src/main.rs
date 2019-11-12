831d1c678533d92582dc169fd7618b2f38dbe0da
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-11-12 09:21:48 +0100

    added function pixel_to_point

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,26 @@ fn  parse_complex(s: &str) -> Option<Complex<f64>> {
     }
 }
 
+///Given x,y in the output image, return the corresponding complex number.
+/// 'bounds' is the width and height of the image
+/// 'pixel' indicates a particular pixel
+/// 'upper_left' and 'lower_right' indicate the area the image covers in the complex
+/// plane
+fn pixel_to_point(  bounds: (usize, usize),
+                    pixel: (usize, usize),
+                    upper_left: Complex<f64>,
+                    lower_right: Complex<f64> )
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                            upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+
 //Tests below
 #[test]
 fn test_parse_pair() {
@@ -53,6 +73,14 @@ fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"),
                Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
 }
 
 
