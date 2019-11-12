0e27740acb8ffc7cb32721a05c36492e954fc500
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-11-12 15:03:02 +0100

    fixed typo, added image crate dependency

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn render(pixels: &mut [u8],
     assert!(pixels.len() == bounds.0 * bounds.1);
     
     for row in 0..bounds.1 {
-        for column in 0.bounds.0 {
+        for column in 0..bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
             pixels[row * bounds.0 + column] = match escape_time(point, 255) {
                 None => 0,
